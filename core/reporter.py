c1f7280e9b4191bc7ce15de85b7efc6da745831a
DanielEstebanDiazYepes <157650339+DanielEstebanDiazYepes@users.noreply.github.com>
2026-08-13 11:09:51 -0500

    arreglo

--- core/reporter.py
+++ core/reporter.py
@@ -11,10 +11,10 @@ class PDFReporter:
     def add_summary(self, stats, graphs_paths):
         self.pdf.cell(200, 10, txt=self.title, ln=True, align='C')
         self.pdf.ln(10)
-        # Añadir estadísticas en tabla simple
+        
         for key, val in stats.items():
             self.pdf.cell(0, 10, txt=f"{key}: {val}", ln=True)
-        # Insertar imágenes de gráficos
+     
         for path in graphs_paths:
             self.pdf.image(path, x=10, w=190)
         return self.pdf.output(dest='S').encode('latin1')
